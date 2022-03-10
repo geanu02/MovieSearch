594672388c54d0e1cf4f92cfbb7b82bc59689914
Gian Carlo Torres <geanu@outlook.com>
2022-03-09 22:14:21 -0500

    M

--- src/movieCard.js
+++ src/movieCard.js
@@ -3,10 +3,12 @@ import React from "react";
 export default function MovieCard({ movie }){
     return (
          <div className="card"  >
+            {/* Movie Poster */}
             <img className="card--image"
                 src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                 alt={movie.title + ' poster'}
                 />
+            {/* Movie Information */}
             <div className="card--content">
                 <h3 className="card--title">{movie.title}</h3>
                 <p><small>RATING: {movie.vote_average}</small></p>
